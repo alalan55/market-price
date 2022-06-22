ba7398130f77036a8919f374fed7f95681c58ff2
aoliveira <aoliveira@keener.io>
2022-06-22 15:30:01 -0300

    Componetiza botão de compra

---
+++ src/components/atoms/ButtonAddProduct.vue
@@ -0,0 +1,48 @@
+<template>
+     <div class="button-product">
+      <div class="title">
+        <span> Adicionar produto </span>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/variables.scss";
+  .button-product {
+    background: white;
+    padding: $padding-1;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    max-width: 900px;
+    margin: 0 auto;
+    -webkit-box-shadow: 5px 5px 12px 5px rgba(0, 0, 0, 0.01);
+    box-shadow: 5px 5px 12px 5px rgba(0, 0, 0, 0.01);
+    transition: 0.2s ease-in-out;
+
+    &:hover {
+      -webkit-box-shadow: 5px 5px 12px 5px rgba(0, 0, 0, 0.04);
+      box-shadow: 5px 5px 12px 5px rgba(0, 0, 0, 0.04);
+    }
+
+    .title {
+      span {
+        font-weight: 600;
+        color: $pink-1;
+      }
+    }
+  }
+
+</style>
--- src/components/templates/HomeTemplate.vue
+++ src/components/templates/HomeTemplate.vue
@@ -1,53 +1,19 @@
 <template>
   <div class="home-template">
-    <div class="button-product">
-      <div class="title">
-        <span> Adicionar produto </span>
-      </div>
-    </div>
+    <ButtonAddProduct />
   </div>
 </template>
 
-<script>
-export default {
-  setup() {},
-};
+<script setup>
+import ButtonAddProduct from "@/components/atoms/ButtonAddProduct.vue";
 </script>
 
 <style lang="scss" scoped>
 @import "@/assets/scss/variables.scss";
+
 .home-template {
-  // border: 2px solid red;
-  // min-height: 100%;
   width: 100%;
   height: 100%;
   padding: $padding-1 $padding-1-5;
-
-  .button-product {
-    background: white;
-    padding: $padding-1;
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    max-width: 900px;
-    margin: 0 auto;
-    -webkit-box-shadow: 5px 5px 12px 5px rgba(0, 0, 0, 0.01);
-    box-shadow: 5px 5px 12px 5px rgba(0, 0, 0, 0.01);
-    transition: 0.2s ease-in-out;
-
-    &:hover {
-      -webkit-box-shadow: 5px 5px 12px 5px rgba(0, 0, 0, 0.04);
-      box-shadow: 5px 5px 12px 5px rgba(0, 0, 0, 0.04);
-    }
-
-    .title {
-      span {
-        font-weight: 600;
-        color: $pink-1;
-      }
-    }
-  }
 }
 </style>
