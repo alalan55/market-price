a7e4b3b71083d13a84803508e23fff7930badd42
alalan55 <alla.ao4975@gmail.com>
2023-04-25 18:24:28 -0300

    Insere criação da tela inicial de login

---
+++ src/components/atoms/mpInput.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({
+  placeholder: { type: String, default: "" },
+  type: { type: String, default: "text" },
+});
+</script>
+
+<template>
+  <div class="inpt">
+    <input :type="props.type" :placeholder="props.placeholder" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/main";
+
+.inpt {
+  width: 100%;
+  height: 45px;
+  input {
+    width: 100%;
+    height: 100%;
+    padding: $space-0-5;
+    border-radius: 10px;
+    border: 1px solid $pink-4;
+
+    &:focus {
+      outline: none;
+    }
+  }
+}
+</style>
--- src/components/templates/Auth/LoginTemplate.vue
+++ src/components/templates/Auth/LoginTemplate.vue
@@ -1,20 +1,52 @@
 <script setup>
 import MpButton from "../../atoms/mpButton2.vue";
+import MpInput from "../../atoms/mpInput.vue";
 </script>
 
 <template>
   <div class="login">
-    <h1>Login</h1>
-
-    <MpButton title="Entrar" />
+    <div class="login__header">
+      <span>Insira as suas informações abaixo para fazer login 😀</span>
+    </div>
+    <div class="login__form">
+      <form>
+        <div>
+          <MpInput placeholder="📧 E-mail" />
+        </div>
+        <div>
+          <MpInput placeholder="🔒 Senha" type="password" />
+        </div>
+      </form>
+    </div>
+    <div class="login__actions">
+      <MpButton title="Entrar" />
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 @import "@/assets/scss/main";
 
-.login{}
+.login {
+  display: flex;
+  flex-direction: column;
+  gap: $space-1;
 
+  &__header {
+    text-align: center;
+    span {
+      font-size: 1.05rem;
+    }
+  }
 
-
+  &__form {
+    form {
+      div {
+        &:not(:last-child) {
+          margin-bottom: $space-1-5;
+        }
+      }
+    }
+  }
+}
 </style>
